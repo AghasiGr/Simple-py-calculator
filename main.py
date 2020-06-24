6865d502976b1c06528870b8a792e048f60d1d06
AghasiGr <66640437+AghasiGr@users.noreply.github.com>
2020-06-24 22:14:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#importing everything we need from colorama
 from colorama import init 
 from colorama import Fore, Back, Style
 
 init()
 print(Fore.BLUE)
+#choosing the operator
 operator = input ("Choose an operator (+, -, *, /, **)")
-
+#choosing the numbers
 a = float(input("Choose the first number:"))
 b = float(input("Choose the second number:"))
-
+#math calculations
 if operator == "+":
     c = a + b
     print("Result: " + str(c))
@@ -24,6 +26,7 @@ elif operator == "/":
 elif operator == "**":
     c = a ** b 
     print("Result: " + str(c))
+#incorrrect configuration
 else:
     print(Fore.RED)
     print("Incorrect configuration!")
