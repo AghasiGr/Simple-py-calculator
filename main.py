9a551d9542b379a5950b406f31de86b45c1bda42
AghasiGr <66640437+AghasiGr@users.noreply.github.com>
2020-07-30 12:44:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 #importing everything we need from colorama
 from colorama import init 
 from colorama import Fore, Back, Style
-
-init()
-print(Fore.BLUE)
-#choosing the operator
-operator = input ("Choose an operator (+, -, *, /, **): ")
+#making a loop
+while True:
+    #activating colorama
+    init()
+    #choosing the operator
+    print(Fore.BLUE)
+    operator = input ("Choose an operator (+, -, *, /, **)")
 #choosing the numbers
-num1 = float(input("Choose the first number: "))
-num2 = float(input("Choose the second number: "))
+    num1 = float(input("Choose the first number:"))
+    num2 = float(input("Choose the second number:"))
 #math calculations
-if operator == "+":
-    num3 = num1 + num2
-    print("Result: " + str(num3))
-elif operator == "-":
-    num3 = num1 - num2
-    print("Result: " + str(num3))
-elif operator == "*":
-    num3 = num1 * num2
-    print("Result: " + str(num3))
-elif operator == "/":
-    num3 = num1 / num2
-    print("Result: " + str(num3))
-elif operator == "**":
-    num3 = num1 ** num2
-    print("Result: " + str(num3))
+    if operator == "+":
+        num3 = num1 + num2
+        print("Result: " + str(num3))
+    elif operator == "-":
+        num3 = num1 - num2
+        print("Result: " + str(num3))
+    elif operator == "*":
+        num3 = num1 * num2
+        print("Result: " + str(num3))
+    elif operator == "/":
+        num3 = num1 / num2
+        print("Result: " + str(num3))
+    elif operator == "**":
+        num3 = num1 ** num2
+        print("Result: " + str(num3))
 #incorrrect configuration
-else:
-    print(Fore.RED)
-    print("Incorrect configuration!")
-    
+    else:
+        print(Fore.RED)
+        
